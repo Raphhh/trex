0c8537f2f7f4f016571808b3984c50ed4a8a21f8
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2013-12-07 14:52:18 +0100

    add IKeyAccessor

---
+++ src/TRex/Iterator/IKeyAccessor.php
@@ -0,0 +1,43 @@
+<?php
+namespace TRex\Iterator;
+
+/**
+ * Interface IKeyAccessor
+ * @package TRex\Iterator
+ */
+interface IKeyAccessor
+{
+
+    /**
+     * Returns all keys.
+     *
+     * @return array
+     */
+	public function getKeys();
+
+    /**
+     * Returns the key associated to the specified offset.
+     * This key may be the offset or an associative key.
+     * Resolves the negative offset.
+     * @example TKeyAccessor::getKey(-1) give the penultimate key.
+     *
+     * @param int $index
+     * @return mixed|null
+     */
+    public function getKey($index);
+
+    /**
+     * Gets the first key.
+     *
+     * @return mixed|null
+     */
+	public function getFirstKey();
+
+    /**
+     * Get the last key.
+     *
+     * @return mixed|null
+     */
+	public function getLastKey();
+
+} 

01a699c884d3ffc6bb040f42f0ee62f62b863740
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2013-12-07 15:15:54 +0100

    add TArrayAccess

---
+++ tests/TRex/Iterator/resources/ArrayAccess.php
@@ -0,0 +1,19 @@
+<?php
+namespace TRex\Iterator\resources;
+
+use TRex\Iterator\TArrayAccess;
+
+/**
+ * Class ArrayAccess
+ * Mock for TArrayAccess.
+ * @package TRex\Iterator\resources
+ */
+class ArrayAccess
+{
+    use TArrayAccess;
+
+    function getIterator()
+    {
+    }
+}
+ 

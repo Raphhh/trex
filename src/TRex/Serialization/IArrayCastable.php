5fc9391731f1bb215ba8390c526edeb90a7e5fb3
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2013-12-20 22:08:18 +0100

    comment correction

--- src/TRex/Serialization/IArrayCastable.php
+++ src/TRex/Serialization/IArrayCastable.php
@@ -12,10 +12,6 @@ interface IArrayCastable
      * Convert an object to an array.
      * The exported array contains all property values ofn the class and its parents, who are not transient.
      *
-     * $filter allows you to filter by visibility properties.
-     * If $filter is true, array keys are composed by the class name dans the property name. If is false, only property name.
-     * If $isRecursive, the conversion also applies to objects in the properties.
-     *
      * @return array
      */
     public function toArray();

414c377538745e1fdee487d0cba9cbb182253950
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2014-01-02 23:16:35 +0100

    Bar has getters

--- tests/TRex/Serialization/resources/Bar.php
+++ tests/TRex/Serialization/resources/Bar.php
@@ -5,6 +5,33 @@ use TRex\Core\Object;
 
 class Bar extends Object
 {
+    /**
+     * @var string
+     */
     private $foo = 'foo from bar';
+
+    /**
+     * @var string
+     */
     private $bar = 'bar from bar';
+
+    /**
+     * Getter of $bar
+     *
+     * @return string
+     */
+    public function getBar()
+    {
+        return $this->bar;
+    }
+
+    /**
+     * Getter of $foo
+     *
+     * @return string
+     */
+    public function getFoo()
+    {
+        return $this->foo;
+    }
 }

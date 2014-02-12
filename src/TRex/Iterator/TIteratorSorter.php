56734186397784b9a106b44cd601255b5a74eedc
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2014-02-12 20:44:50 +0100

    init IteratorSorterOrderBy

--- src/TRex/Iterator/TIteratorSorter.php
+++ src/TRex/Iterator/TIteratorSorter.php
@@ -57,4 +57,46 @@ trait TIteratorSorter
     {
         return new $this(array_reverse($this->getIterator()->toArray(), $areKeysPreserved));
     }
+
+    function orderBy(array $orders)
+    {
+        $values = $this->getIterator()->toArray();
+        usort($values, $this->getOrderByClosure($orders));
+        return new $this($values);
+    }
+
+    private function getOrderByClosure(array $orders)
+    {
+        return \Closure::bind(
+            function ($refData, $compData) use ($orders) {
+                return $this->inspectOrdering($refData, $compData, $orders);
+            },
+            $this,
+            get_class($this)
+        );
+    }
+
+    private function inspectOrdering($refData, $compData, array $orders)
+    {
+        foreach ($orders as $key => $order) {
+            if ($this->getValue($refData, $key) !== $this->getValue($compData, $key)) {
+                if ($this->getValue($refData, $key) < $this->getValue($compData, $key)) {
+                    return $order * (-1);
+                }
+                return $order;
+            }
+        }
+        return 0;
+    }
+
+    private function getValue($data, $key)
+    {
+        if (is_object($data)) {
+            return $data->{'get' . $key}();
+        }
+        if (is_array($data)) {
+            return $data[$key];
+        }
+        throw new \LogicException(sprintf('Values must be array or object. %s received.', gettype($data)));
+    }
 }
